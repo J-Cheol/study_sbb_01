3e93c5d04cee8c86de40f6d60222318df12cd3e3
전화철 <lunaris0206@naver.com>
2024-02-23 16:47:49 +0900

    Initial commit

---
+++ src/main/java/com/ll/study_sbb_01/StudySbb01Application.java
@@ -0,0 +1,15 @@
+package com.ll.study_sbb_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySbb01Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StudySbb01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/study_sbb_01/StudySbb01ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.study_sbb_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySbb01ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
