3e15565d00d8dd4eb2c1bd53c3a5d94d6654253f
전화철 <lunaris0206@naver.com>
2024-03-22 11:33:01 +0900

    회원 웹 기능 - 홈 화면 추가

---
+++ src/main/java/com/ll/study_sbb_01/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ll.study_sbb_01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
